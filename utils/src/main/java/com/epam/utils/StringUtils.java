ed3b6e222e664d00be91c71a300f36ef4f29bd04
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-04-02 20:02:52 +0300

    Update StringUtils.java

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,8 +2,6 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-               if (str.length() > 0)
-        return false;
-        else return true;
+return false;
     }
 }
